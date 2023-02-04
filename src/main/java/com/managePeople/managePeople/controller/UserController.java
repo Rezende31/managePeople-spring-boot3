3bc41fac8e916fd75686ef5a819d53303baa93a5
Leonardo Rezende <leo.rezende31@gmail.com>
2023-02-04 16:10:45 -0300

    User insert 201

--- src/main/java/com/managePeople/managePeople/controller/UserController.java
+++ src/main/java/com/managePeople/managePeople/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.managePeople.managePeople.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.managePeople.managePeople.model.User;
 import com.managePeople.managePeople.services.UserService;
@@ -31,4 +35,15 @@ public class UserController {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(user.getId()).toUri() ;
+		
+		return ResponseEntity.created(uri).body(user) ;
+	}
+	
 }
