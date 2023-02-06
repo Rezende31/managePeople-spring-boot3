c178bd076b6c25ac8ced6d5886712701a80a433c
Leonardo Rezende <leo.rezende31@gmail.com>
2023-02-06 19:54:00 -0300

    test unitario update + insert (User)

--- src/test/java/com/managePeople/managePeople/controllerTest/UserControllerTest.java
+++ src/test/java/com/managePeople/managePeople/controllerTest/UserControllerTest.java
@@ -2,7 +2,6 @@ package com.managePeople.managePeople.controllerTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,6 +14,7 @@ import org.junit.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.managePeople.managePeople.controller.UserController;
 import com.managePeople.managePeople.model.User;
+import com.managePeople.managePeople.repositories.UserRepository;
 import com.managePeople.managePeople.services.UserService;
 
 @RunWith(SpringRunner.class)
@@ -35,6 +36,9 @@ public class UserControllerTest {
 
 	@MockBean
 	private UserService userService;
+	
+	@Mock
+	private UserRepository userRepository;
 
 	@Test
 	public void testFindAllUsers() throws Exception {
@@ -63,6 +67,17 @@ public class UserControllerTest {
 		when(userService.insert(user)).thenReturn(user);
 		assertThat(user.getName()).isEqualTo("@test");
 		assertThat(user).isNotNull();
+	}
+	
+	@Test
+	public void testUpdateUser() {
+		User user = new User(1L, "@testUpdate", null);
+		when(userService.insert(user)).thenReturn(user);
+		when(userRepository.findById(1L)).thenReturn(java.util.Optional.of(user));
+		
+		
+		assertThat(user).isNotNull();
+		assertThat(user.getId()).isEqualTo(1L);
 	}
 
 }
